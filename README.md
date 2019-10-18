00e88ab0fb16ba0af8e51fb48e4dde2bef5506a5
NISH1001 <nishanpantha@gmail.com>
2019-10-18 08:27:47 +0545

    Add a new aligner based on bounding box.
    Add `aligner.py` to align images.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # image-alignment
 A naive way to align a test image with a template one
+
+## Usage
+Run `aligner.py` with image name supplied as argument.  
+
+```bash
+python aligner.py images/test.jpg
+```
+
+The result will be stored as `res.jpg`
+
+Random images can be tested by supplying `random` as an argument.  
+```bash
+python aligner.py random
+```
+
+## How?
+The algorithm is very simple.  
+- binarize/threshold the original image (with foreground color as white)
+- take the coordinates of white pixels
+- simply deduce a bounding box that enclosed these pixels along with the angle of alignment of the box.
+- calculate angle of rotation/alignment
+- align the origina image
+
+The process is very naive (and simple). So, if the origina image if already rotated by 90 degrees perfectly, the alignment doesn't work.  
--- aligner.py
+++ aligner.py
@@ -1,53 +1,92 @@
 #!/usr/bin/env python3
 
+import cv2
 import numpy as np
-from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+import glob
+import random
 
-BASEWIDTH = 50
+import sys
 
-def resize(img):
-    wpercent = ( BASEWIDTH / float(img.size[0]) )
-    hsize = int((float(img.size[1])*float(wpercent)))
-    return img.resize((BASEWIDTH,BASEWIDTH), Image.ANTIALIAS)
+def random_file(path):
+    return random.choice(glob.glob(path))
+
+
+def convert_to_gray(img):
+    print("Converting to grayscale...")
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    return cv2.bitwise_not(gray)
 
 def binarize(img):
-    return img.convert('L')
+    print("Binarizing...")
+    return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-def load_image(filename):
-    img = Image.open(filename)
-    img = resize(img)
-    img = binarize(img)
-    data = np.copy(np.asarray(img))
-    data[data<25] = 0
-    return data
+def calculate_bbox(img):
+    print("Calculating bounding box based on white pixels...")
+    coords = np.column_stack(np.where(img> 0))
+    return cv2.minAreaRect(coords)
 
-def calculate_dist(x1, x2):
-    return np.average(x1 - x2)
+def draw_bbox(img, rect):
+    print("Drawing bounding box over the image...")
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    img2 = img.copy()
+    cv2.drawContours(img2, [box], 0, (255, 255, 255), 2)
+    plot(img2)
 
-def pixels_to_pil(pixels):
-    return Image.fromarray(pixels)
+def calculate_angle(rect):
+    print("Calculating rotation angle...")
+    angle = rect[-1]
+    return -(90 + angle) if angle < -45 else -angle
 
-def pil_to_pixels(img):
-    return np.array(img)
+def align(image, angle):
+    print("Aligning image with angle=[{}]".format(angle))
+    (h, w) = image.shape[:2]
+    # rotation center
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    return cv2.warpAffine(image, M, (w, h),
+        flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
-def find_nearest_alignment(template, test):
-    img = pixels_to_pil(test)
-    res = []
-    for degree in range(0, 360, 45):
-        img_rot = img.rotate(degree)
-        dist = calculate_dist(template, pil_to_pixels(img_rot))
-        res.append( (degree, dist) )
-    nearest = min(res, key = lambda t : t[1])
-    return nearest
+def plot(img):
+    if img.ndim > 2:
+        plt.imshow(img)
+    else:
+        plt.imshow(img, 'gray')
+    plt.show()
 
 def main():
-    template = load_image("data/template.jpeg")
-    test = load_image("data/test.jpeg")
-    alignment = find_nearest_alignment(template, test)
-    img = pixels_to_pil(test)
-    img_res = img.rotate(alignment[0])
-    img_res.save("data/res.jpeg")
+    args = sys.argv[1:]
+    if not args:
+        print("Please supply image path as an argument.\nUsage: python align.py <image_path>")
+    # filename = "images/misaligned_images/test.jpg"
+    # filename = random_file('images/*')
+    filename = args[0].strip()
+    filename = random_file('images/*') if filename == 'random' else filename
+    print(filename)
 
+    image = cv2.imread(filename)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plot(image)
+
+    gray = convert_to_gray(image)
+    plot(gray)
+
+    thresh = binarize(gray)
+    plot(thresh)
+
+    rect = calculate_bbox(thresh)
+    draw_bbox(thresh, rect)
+
+    angle = calculate_angle(rect)
+    print(angle)
+    if -0.2 < angle < 0.2:
+        print("Already aligned. Aborting...")
+        return
+
+    final = align(image, angle)
+    cv2.imwrite('res.jpg', final)
+    plot(final)
 
 if __name__ == "__main__":
     main()
